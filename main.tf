9cb8c7e74fe9c9f51874c56bd99eec2826f1df17
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-04-30 15:46:03 -0300

    fix datasource

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_appsync_datasource" "example_1" {
   count            = "${var.type == "NONE" ? 1 : 0}"
-  api_id           = "${aws_appsync_graphql_api.example_1.id}"
+  api_id           = "${aws_appsync_api_key.example_1.id}"
   name             = "tf_appsync_example"
   service_role_arn = "${module.iam_role.example.arn}"
   type             = "${var.type}"
@@ -8,7 +8,7 @@ resource "aws_appsync_datasource" "example_1" {
 
 resource "aws_appsync_datasource" "example_2" {
   count            = "${var.type == "AMAZON_DYNAMODB" ? 1 : 0}"
-  api_id           = "${aws_appsync_graphql_api.example_1.id}"
+  api_id           = "${aws_appsync_api_key.example_1.id}"
   name             = "tf_appsync_example"
   service_role_arn = "${module.iam_role.example.arn}"
   type             = "${var.type}"
@@ -18,7 +18,7 @@ resource "aws_appsync_datasource" "example_2" {
 
 resource "aws_appsync_datasource" "example_3" {
   count            = "${var.type == "AWS_LAMBDA" ? 1 : 0}"
-  api_id           = "${aws_appsync_graphql_api.example_1.id}"
+  api_id           = "${aws_appsync_api_key.example_1.id}"
   name             = "tf_appsync_example"
   service_role_arn = "${module.iam_role.example.arn}"
   type             = "${var.type}"
@@ -27,7 +27,7 @@ resource "aws_appsync_datasource" "example_3" {
 
 resource "aws_appsync_datasource" "example_4" {
   count            = "${var.type == "AMAZON_ELASTICSEARCH" ? 1 : 0}"
-  api_id           = "${aws_appsync_graphql_api.example_1.id}"
+  api_id           = "${aws_appsync_api_key.example_1.id}"
   name             = "tf_appsync_example"
   service_role_arn = "${module.iam_role.example.arn}"
   type             = "${var.type}"
@@ -36,7 +36,7 @@ resource "aws_appsync_datasource" "example_4" {
 
 resource "aws_appsync_datasource" "example_5" {
   count            = "${var.type == "HTTP" ? 1 : 0}"
-  api_id           = "${aws_appsync_graphql_api.example_1.id}"
+  api_id           = "${aws_appsync_api_key.example_1.id}"
   name             = "tf_appsync_example"
   service_role_arn = "${module.iam_role.example.arn}"
   type             = "${var.type}"
